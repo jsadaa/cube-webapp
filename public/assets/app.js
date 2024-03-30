53b0066f3461b5648c1433903492928d49163a3e
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2024-03-30 17:58:46 +0100

    add register form

--- public/assets/app.js
+++ public/assets/app.js
@@ -17,16 +17,14 @@ $('#filter-button').click(function() {
     var region = $('#region option:selected:not(:disabled)').val() || '';
     var famille = $('#famille option:selected:not(:disabled)').val() || '';
     var fournisseur = $('#fournisseur option:selected:not(:disabled)').val() || '';
-    
-    // now, for each element with class "card", we check if its chilren with classes "appellation", "cepage", "region", "famille" and "fournisseur" match the selected values (it could be "all", a specific value or an empty string)
+
     $('.card').each(function() {
         var appellationMatch = $(this).find('.appellation .value').text().trim() === appellation || appellation === 'Tous' || appellation === '';
         var cepageMatch = $(this).find('.cepage .value').text().trim() === cepage || cepage === 'Tous' || cepage === '';
         var regionMatch = $(this).find('.region .value').text().trim() === region || region === 'Tous' || region === '';
         var familleMatch = $(this).find('.famille .value').text().trim() === famille || famille === 'Tous' || famille === '';
         var fournisseurMatch = $(this).find('.fournisseur .value').text().trim() === fournisseur || fournisseur === 'Tous' || fournisseur === '';
-        
-        // if all the conditions are met, we show the card, otherwise we hide it
+
         if (appellationMatch && cepageMatch && regionMatch && familleMatch && fournisseurMatch) {
             $(this).show();
         } else {
--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -12,12 +12,10 @@ class AuthController extends AbstractController
     #[Route('/login', name: 'login')]
     public function index(Request $request): Response
     {
-        // Récupérer l'erreur d'authentification de la session s'il y en a une
         $session = $request->getSession();
         $error = $session->get('auth_error');
-        $session->remove('auth_error'); // Effacer l'erreur de la session après l'avoir récupérée
-        
-        // Récupérer le dernier nom d'utilisateur saisi (si nécessaire)
+        $session->remove('auth_error');
+
         $lastUsername = $session->get('last_username');
         $session->remove('last_username'); // Effacer le dernier nom d'utilisateur de la session après l'avoir récupéré
 
